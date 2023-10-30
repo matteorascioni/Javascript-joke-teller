a465d3558c2bdc116c7e39090079e85e61783a11
Matteo <matteorascioni@gmail.com>
2023-10-30 13:23:06 +0100

    Javascript joke Teller

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Project Name
+
+This is a little app where a voice will tell you a joke from a joke API
+
+## Thoughts
+
+I have created text to speech projects in the past. This one was different in that it used a joke API to speak the joke. This was a pretty interesting project. It's a neat little app that can be fun for a few minutes listening to your computer tell you some jokes.
+
+## My Cusomizations
+
+Something I added myself was three options on the page to let the user choose the kinds of jokes they want. This can give the user a little more customizations on what they want to listen to.
+
+## Running this Locally
+
+1. Go to the documentation page for the Voice RSS Text to Speech API page found [here](https://www.voicerss.org/)
+2. Click on 'API Key' and then 'Get Api Key'.
+3. Create a free account.
+4. When your account is created, you need to activate the account by clicking the 'Activate Account' button on the sign up page or by email.
+5. Once your account is activated, you can copy your API key and paste it into the script.js file.
+   - Line 6, insert in 'APY_KEY' your API key contained in single quotes.
---
+++ script.js
@@ -0,0 +1,79 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+const programmingButton = document.getElementById('programming');
+const darkButton = document.getElementById('dark');
+const nsfwButton = document.getElementById('nsfw');
+const APY_KEY = 'f94b5b70ec03467488be47499701d42a';
+
+let currentAPIUrl = 'https://sv443.net/jokeapi/v2/joke/Programming,Dark?blacklistFlags=nsfw,religious,political,racist,sexist';
+
+// Disable / Enable button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Pass the joke to the voiceRSS API
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: APY_KEY,
+    src: joke,
+    hl: 'en-us',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+  let joke = '';
+
+  try {
+    const response = await fetch(currentAPIUrl);
+    const data = await response.json();
+
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+
+    // Tell the joke
+    tellMe(joke);
+
+    // Disable the button
+    toggleButton();
+  } catch (error) {
+    console.log('An error occured in getJokes(). ', error);
+  }
+}
+
+// Event Listeners
+button.addEventListener('click', () => {
+  getJokes();
+});
+audioElement.addEventListener('ended', toggleButton);
+
+// These are to let the user switch between different joke types.
+programmingButton.addEventListener('click', () => {
+  darkButton.disabled = false;
+  nsfwButton.disabled = false;
+  programmingButton.disabled = true;
+  currentAPIUrl =
+    'https://sv443.net/jokeapi/v2/joke/Programming,Dark?blacklistFlags=nsfw,religious,political,racist,sexist';
+});
+darkButton.addEventListener('click', () => {
+  darkButton.disabled = true;
+  nsfwButton.disabled = false;
+  programmingButton.disabled = false;
+  currentAPIUrl =
+    'https://sv443.net/jokeapi/v2/joke/Dark?blacklistFlags=nsfw,religious,political,racist,sexist';
+});
+nsfwButton.addEventListener('click', () => {
+  darkButton.disabled = false;
+  nsfwButton.disabled = true;
+  programmingButton.disabled = false;
+  currentAPIUrl =
+    'https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=religious,political,racist,sexist';
+});
